7cdcf22ae25e77258ee6523a4cb8c24617fa766b
Lucas Dutra <ldutra98@gmail.com>
2019-10-30 01:40:12 -0300

    pax-app/Wiki#189 Add pax inserts

---
+++ Inserts/Pax_Inserts.sql
@@ -0,0 +1,101 @@
+use paxdb;
+
+INSERT INTO
+    PAX (
+        date,
+        description,
+        name,
+        price,
+        status,
+        user_id,
+        provider_id,
+        chat_id,
+        address_id
+    )
+VALUES
+    (
+        '2019-10-10',
+        'Descrição',
+        'conserto de celular',
+        10.5,
+        'P',
+        1,
+        1,
+        1,
+        6
+    );
+
+INSERT INTO
+    PAX (
+        date,
+        description,
+        name,
+        price,
+        status,
+        user_id,
+        provider_id,
+        chat_id,
+        address_id
+    )
+VALUES
+    (
+        '2019-10-10',
+        'Descrição',
+        'conserto de carro',
+        10.5,
+        'I',
+        1,
+        1,
+        1,
+        6
+    );
+
+INSERT INTO
+    PAX (
+        date,
+        description,
+        name,
+        price,
+        status,
+        user_id,
+        provider_id,
+        chat_id,
+        address_id
+    )
+VALUES
+    (
+        '2019-10-10',
+        'Descrição',
+        'conserto de casa',
+        10.5,
+        'C',
+        1,
+        1,
+        1,
+        6
+    );
+
+INSERT INTO
+    PAX (
+        date,
+        description,
+        name,
+        price,
+        status,
+        user_id,
+        provider_id,
+        chat_id,
+        address_id
+    )
+VALUES
+    (
+        '2019-10-10',
+        'Descrição',
+        'conserto de máquina',
+        10.5,
+        'F',
+        1,
+        1,
+        1,
+        6
+    );
